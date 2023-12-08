ab456210389d62f132831a70800b4c511338f202
Harry-Kwesi <harrydegraft@hotmail.com>
2023-12-08 11:57:50 +0530

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Harry Kwesi De Graft on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
